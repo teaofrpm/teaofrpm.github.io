49d2430be8986f2d4873dfc7eae029ae1fd636c3
teaofrpm <saharsb1988@gmail.com>
2026-09-26 10:19:01 +0530

    Add ICE servers to TEAOFRPM_CONFIG
    Added ICE servers configuration for WebRTC.

--- js/config.js
+++ js/config.js
@@ -1,4 +1,3 @@
-
 window.TEAOFRPM_CONFIG = {
   SUPABASE_URL: "https://jfybtgoofxfhjgeyumzl.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpmeWJ0Z29vZnhmaGpnZXl1bXpsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkyOTI3NzIsImV4cCI6MjEwNDg2ODc3Mn0.9tScr8yYRwBR32Wyj6UO36kQlTwSuJBcABiNtVGhCZA",
@@ -6,4 +5,12 @@ window.TEAOFRPM_CONFIG = {
   AUTH_EMAIL_DOMAIN: "teaofrpm.local",
 
   ROOM_NAME: "𝑅𝒫𝑀 𝒫𝓊𝒷𝓁𝒾𝒸 𝑅𝑜𝑜𝓂",
+
+  ICE_SERVERS: [
+    { urls: "stun:stun.relay.metered.ca:80" },
+    { urls: "turn:global.relay.metered.ca:80", username: "68049b3a6dd11bf3ac7694e1", credential: "mmtrzLtF00iTdXnw" },
+    { urls: "turn:global.relay.metered.ca:80?transport=tcp", username: "68049b3a6dd11bf3ac7694e1", credential: "mmtrzLtF00iTdXnw" },
+    { urls: "turn:global.relay.metered.ca:443", username: "68049b3a6dd11bf3ac7694e1", credential: "mmtrzLtF00iTdXnw" },
+    { urls: "turns:global.relay.metered.ca:443?transport=tcp", username: "68049b3a6dd11bf3ac7694e1", credential: "mmtrzLtF00iTdXnw" },
+  ],
 };
